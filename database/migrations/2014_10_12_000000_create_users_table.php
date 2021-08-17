8904327a1c4ac12e4125ed36f60e7910b2e23b04
Canoso <jpcanoso@hotmail.com>
2021-08-17 22:40:54 +0100

    Added admin role

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,8 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->boolean('admin')->default(false);
+            $table->ipAddress('last_ip_address');
             $table->rememberToken();
             $table->timestamps();
         });
