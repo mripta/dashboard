fa6f6ecf0b9f7522f04c4ef1f4b41402b0557d07
Canoso <jpcanoso@hotmail.com>
2021-09-01 01:16:20 +0100

    Added seeders to ref and param

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,9 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             UserSeeder::class,
-            TeamSeeder::class
+            TeamSeeder::class,
+            RefSeeder::class,
+            ParamSeeder::class
         ]);
     }
 }
--- database/seeders/TeamSeeder.php
+++ database/seeders/TeamSeeder.php
@@ -14,6 +14,7 @@ class TeamSeeder extends Seeder
      */
     public function run()
     {
+        // team 1
         $team = new Team();
         $team->name = 'Equipa 1';
         $team->description = 'Ponto de recolha de Tomar';
@@ -25,5 +26,16 @@ class TeamSeeder extends Seeder
         $team->users()->attach(1, ['owner' => 1]);
         // add user id 2 to the team
         $team->users()->attach(2);
+    
+        // team 2
+        $team = new Team();
+        $team->name = 'Equipa 2';
+        $team->description = 'Ponto de recolha de Lisboa';
+        $team->username = 'equipa2';
+        $team->password = bcrypt('password');
+        $team->save();
+
+        // add user id 2 to the team with ownership 
+        $team->users()->attach(2, ['owner' => 1]);
     }
 }
