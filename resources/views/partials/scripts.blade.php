5e443aa2c2078a1bf3bd18ec1e15d001b5b79aa6
Canoso <jpcanoso@hotmail.com>
2021-09-05 20:10:54 +0100

    Added daterangepicker assets

--- resources/views/partials/scripts.blade.php
+++ resources/views/partials/scripts.blade.php
@@ -3,8 +3,11 @@
 <script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
 <script src="{{ asset('js/adminlte.min.js') }}"></script>
 <script src="{{ asset('js/chart.min.js') }}"></script>
+<script src="{{ asset('js/moment.min.js') }}"></script>
+<script src="{{ asset('js/daterangepicker.js') }}"></script>
 
 <script>
+// select light or dar theme on OS settings
 if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
     $('body').addClass("dark-mode");
     $('.wrapper').find(".navbar").removeClass("navbar-light");
