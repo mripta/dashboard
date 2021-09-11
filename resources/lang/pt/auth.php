fd7c0802606c2467e395c85d5a9b39dd7bfb507a
Canoso <jpcanoso@hotmail.com>
2021-09-11 17:41:34 +0100

    Added Portuguese Translations
    Changed project main language

---
+++ resources/lang/pt/auth.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Utilizador ou password errados.',
+    'password' => 'A palavra-passe está incorreta.',
+    'throttle' => 'Demasiadas tentativas de login. Tente de novo em :seconds segundos.',
+
+];
